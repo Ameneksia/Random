af6a7cdffb2cd50760f5a210f6c7ab611b9d4bc2
Anastasia <ameneksia@gmail.com>
2017-11-17 18:06:40 +0300

    3activity

--- app/src/main/java/study/vv/random/MainActivity.java
+++ app/src/main/java/study/vv/random/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     public void plus(View v){
         EditText num1 = (EditText)findViewById(R.id.first_number);
         EditText num2 = (EditText)findViewById(R.id.second_number);
-        TextView res = (TextView)findViewById(R.id.second_number);
+        TextView res = (TextView)findViewById(R.id.result);
         String snum1 = num1.getText().toString();
         String snum2 = num2.getText().toString();
         res.setText("" + (Integer.parseInt(snum1) +
